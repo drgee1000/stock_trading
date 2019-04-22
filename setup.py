1216df039b8fe017d3c320eef3687cc28cf0a5a4
XianfanGu <529632102@qq.com>
2019-04-22 12:27:55 +0800

    initial and create the responsitory

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    # Needed to silence warnings (and to be a worthwhile package)
+    name='toollib',
+    url='https://github.com/XianfanGu/toollib',
+    author='Xianfan Gu',
+    author_email='xianfang@asu.edu',
+    # Needed to actually package something
+    packages=['toollib'],
+    # Needed for dependencies
+    install_requires=['talib','pandas','zipline','sklearn','numpy>=1.16.2'],
+    # *strongly* suggested for sharing
+    version='0.1',
+    # The license can be anything you like
+    license='MIT',
+    # We will also need a readme eventually (there will be a warning)
+    # long_description=open('README.txt').read(),
+)
