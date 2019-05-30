493d206302c29a595e5091ed2f36eb8cc298459c
XianfanGu <529632102@qq.com>
2019-05-30 11:21:03 +0800

    revision

--- toollib/Neat/fitness.py
+++ toollib/Neat/fitness.py
@@ -16,117 +16,142 @@ class Fitness:
     #acc
     def eval_genomes1(self,genomes,config):
         #print('The data index is %d',data_idx)
-        eval_input=self.inputs[self.data_idx:self.data_idx+self.intervals]
-        eval_output=self.outputs[self.data_idx:self.data_idx+self.intervals]
-        if(self.data_idx <= self.size):
+
+        if((self.data_idx + self.intervals) <= self.size):
+            eval_input = self.inputs[self.data_idx:self.data_idx + self.intervals]
+            eval_output = self.outputs[self.data_idx:self.data_idx + self.intervals]
             self.data_idx = self.data_idx + self.intervals
-            for genome_id, genome in genomes:
-                AUX = 0.0
-                net = neat.nn.FeedForwardNetwork.create(genome, config)
-                pred=[]
-                eval=[]
-                for i,o in zip(eval_input,eval_output):
-                    output = net.activate(i)
-                    aux = np.argmax(output)
-                    pred.append(aux)
-                    eval.append(np.argmax(o))
-                result=self.accuracy_function(eval,pred)
-                genome.fitness = result[0]
+        else:
+            eval_input = self.inputs[self.data_idx:]
+            eval_output = self.outputs[self.data_idx:]
+            self.data_idx = 0
+
+        for genome_id, genome in genomes:
+            AUX = 0.0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            pred=[]
+            eval=[]
+            for i,o in zip(eval_input,eval_output):
+                output = net.activate(i)
+                aux = np.argmax(output)
+                pred.append(aux)
+                eval.append(np.argmax(o))
+            result=self.accuracy_function(eval,pred)
+            genome.fitness = result[0]
+
 
     #badacc+goodacc
     def eval_genomes2(self,genomes,config):
         #print('The data index is %d',data_idx)
-        eval_input=self.inputs[self.data_idx:self.data_idx+self.intervals]
-        eval_output=self.outputs[self.data_idx:self.data_idx+self.intervals]
-        if(self.data_idx <= self.size):
+
+        if((self.data_idx + self.intervals) <= self.size):
+            eval_input = self.inputs[self.data_idx:self.data_idx + self.intervals]
+            eval_output = self.outputs[self.data_idx:self.data_idx + self.intervals]
             self.data_idx = self.data_idx + self.intervals
-            for genome_id, genome in genomes:
-                AUX = 0.0
-                net = neat.nn.FeedForwardNetwork.create(genome, config)
-                pred=[]
-                eval=[]
-                for i,o in zip(eval_input,eval_output):
-                    output = net.activate(i)
-                    aux = np.argmax(output)
-                    pred.append(aux)
-                    eval.append(np.argmax(o))
-                result=self.accuracy_function(eval,pred)
-                genome.fitness = result[1]+result[2]
-                #genome.fitness = result[0]
+        else:
+            eval_input = self.inputs[self.data_idx:]
+            eval_output = self.outputs[self.data_idx:]
+            self.data_idx = 0
+
+        for genome_id, genome in genomes:
+            AUX = 0.0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            pred=[]
+            eval=[]
+            for i,o in zip(eval_input,eval_output):
+                output = net.activate(i)
+                aux = np.argmax(output)
+                pred.append(aux)
+                eval.append(np.argmax(o))
+            result=self.accuracy_function(eval,pred)
+            genome.fitness = result[1]+result[2]
+            #genome.fitness = result[0]
 
     #profit
     def eval_genomes3(self,genomes,config):
         #print('The data index is %d',data_idx)
-        eval_input=self.inputs[self.data_idx:self.data_idx+self.intervals]
-        eval_output=self.outputs[self.data_idx:self.data_idx+self.intervals]
-        if(self.data_idx <= self.size):
+
+        if((self.data_idx + self.intervals) <= self.size):
+            eval_input = self.inputs[self.data_idx:self.data_idx + self.intervals]
+            eval_output = self.outputs[self.data_idx:self.data_idx + self.intervals]
             self.data_idx = self.data_idx + self.intervals
-            for genome_id, genome in genomes:
-                AUX = 0.0
-                net = neat.nn.FeedForwardNetwork.create(genome, config)
-                pred=[]
-                eval=[]
-                for i,o in zip(eval_input,eval_output):
-                    output = net.activate(i)
-                    aux = np.argmax(output)
-                    pred.append(aux)
-                    eval.append(np.argmax(o))
+        else:
+            eval_input = self.inputs[self.data_idx:]
+            eval_output = self.outputs[self.data_idx:]
+            self.data_idx = 0
 
-                profit=0
-                for i in range(len(eval)):
-                    result=eval[i]
-                    #print(eval_input[i],eval_output[i],result)
-                    apply_money=eval_input[i][0]
-                    return_money=eval_input[i][1]*eval_input[i][2]
-                    if result==pred[i]:
-                        if result==0:
-                            profit=profit+apply_money
-                        else:
-                            profit=profit+return_money-apply_money
+        for genome_id, genome in genomes:
+            AUX = 0.0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            pred=[]
+            eval=[]
+            for i,o in zip(eval_input,eval_output):
+                output = net.activate(i)
+                aux = np.argmax(output)
+                pred.append(aux)
+                eval.append(np.argmax(o))
+
+            profit=0
+            for i in range(len(eval)):
+                result=eval[i]
+                #print(eval_input[i],eval_output[i],result)
+                apply_money=eval_input[i][0]
+                return_money=eval_input[i][1]*eval_input[i][2]
+                if result==pred[i]:
+                    if result==0:
+                        profit=profit+apply_money
                     else:
-                        if result==0:
-                            profit=profit-apply_money
-                        else:
-                            profit=profit-(return_money-apply_money)
+                        profit=profit+return_money-apply_money
+                else:
+                    if result==0:
+                        profit=profit-apply_money
+                    else:
+                        profit=profit-(return_money-apply_money)
 
-                genome.fitness = profit
+            genome.fitness = profit
 
     #proft+history
     def eval_genomes4(self,genomes,config):
         #print('The data index is %d',data_idx)
-        eval_input=self.inputs[self.data_idx:self.data_idx+self.intervals]
-        eval_output=self.outputs[self.data_idx:self.data_idx+self.intervals]
-        if(self.data_idx <= self.size):
-            self.data_idx = self.data_idx + self.intervals
-            for genome_id, genome in genomes:
-                AUX = 0.0
-                net = neat.nn.FeedForwardNetwork.create(genome, config)
-                pred=[]
-                eval=[]
-                for i,o in zip(eval_input,eval_output):
-                    output = net.activate(i)
-                    aux = np.argmax(output)
-                    pred.append(aux)
-                    eval.append(np.argmax(o))
 
-                profit=0
-                for i in range(len(eval)):
-                    result=eval[i]
-                    #print(eval_input[i],eval_output[i],result)
-                    apply_money=eval_input[i][0]
-                    return_money=eval_input[i][1]*eval_input[i][2]
-                    if result==pred[i]:
-                        if result==0:
-                            profit=profit+apply_money
-                        else:
-                            profit=profit+return_money-apply_money
+        if((self.data_idx + self.intervals) <= self.size):
+            eval_input = self.inputs[self.data_idx:self.data_idx + self.intervals]
+            eval_output = self.outputs[self.data_idx:self.data_idx + self.intervals]
+            self.data_idx = self.data_idx + self.intervals
+        else:
+            eval_input = self.inputs[self.data_idx:]
+            eval_output = self.outputs[self.data_idx:]
+            self.data_idx = 0
+
+        for genome_id, genome in genomes:
+            AUX = 0.0
+            net = neat.nn.FeedForwardNetwork.create(genome, config)
+            pred=[]
+            eval=[]
+            for i,o in zip(eval_input,eval_output):
+                output = net.activate(i)
+                aux = np.argmax(output)
+                pred.append(aux)
+                eval.append(np.argmax(o))
+
+            profit=0
+            for i in range(len(eval)):
+                result=eval[i]
+                #print(eval_input[i],eval_output[i],result)
+                apply_money=eval_input[i][0]
+                return_money=eval_input[i][1]*eval_input[i][2]
+                if result==pred[i]:
+                    if result==0:
+                        profit=profit+apply_money
                     else:
-                        if result==0:
-                            profit=profit-apply_money
-                        else:
-                            profit=profit-(return_money-apply_money)
-                genome.pastfitness=genome.fitness if genome.fitness !=None else 0
-                genome.fitness = self.arg[0]*profit+self.arg[1]*genome.pastfitness
+                        profit=profit+return_money-apply_money
+                else:
+                    if result==0:
+                        profit=profit-apply_money
+                    else:
+                        profit=profit-(return_money-apply_money)
+            genome.pastfitness=genome.fitness if genome.fitness !=None else 0
+            genome.fitness = self.arg[0]*profit+self.arg[1]*genome.pastfitness
 
     def eval_test_next(self,genome,idx,intervals,config):
         acc=[]
